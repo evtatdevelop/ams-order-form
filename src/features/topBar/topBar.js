cb8bd2de119831006c4821085b48eebcd1e6637c
Evgenii Tatarenko <evtatdevelop@gmail.com>
2024-02-27 16:21:55 +0700

    styles for Preferences bar

--- src/features/topBar/topBar.js
+++ src/features/topBar/topBar.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './topBar.module.scss';
 import { useSelector } from "react-redux";
 import { user, loading } from '../user/userSlice';
 import { LangButton } from "../components/langButton/langButton";
 import dictionary from '../../dictionary.json';
 import { ThemeButton } from "../components/themeButton/themeButton";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export const TopBar = () => {
   const userData = useSelector(user);
   const load = useSelector(loading);
+
+  const [show, setShow] = useState(false);
 
   return (
     <header className={styles.header}>
     { !load
       ? <>
           <h1>{ dictionary.ams_order_form[userData['lang']] }</h1>
-          <div className={styles.preferences}>
-            <div><LangButton/></div>
-            <div><ThemeButton/></div>
-          </div>
+
+          { show
+            ? <div className={styles.preferences}>
+                <div><LangButton/></div>
+                <div><ThemeButton/></div>
+              </div>
+            : <button type="button" className={styles.btnPreferences}
+                onClick={() => setShow(true)}
+              >
+                <FontAwesomeIcon icon={ faBars } className={styles.btnPreferencesIcon} />
+              </button>  
+          }
 
         </>
       : null
