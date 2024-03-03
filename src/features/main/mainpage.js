59657f4e849e066ed525c872de8f3707bd947082
Evgenii Tatarenko <evtatdevelop@gmail.com>
2024-03-03 10:17:51 +0700

    L0oad Page

--- src/features/main/mainpage.js
+++ src/features/main/mainpage.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from "react";
 import styles from './mainpage.module.scss';
 import { useSelector, useDispatch } from "react-redux";
-import { user } from '../user/userSlice';
+import { user, loading } from '../user/userSlice';
 import ExpirationScreen from "../expirationScreen";
 import { darkTheme } from "./mainpageSlice";
 import { TopBar } from "../topBar/topBar";
+import { changeTheme } from "./mainpageSlice";
+import { LoadPage } from "../loadPage/loadPage";
 
 export const MainPage = () => {
   const dispatch = useDispatch();
   const userData = useSelector(user);
   const dark = useSelector(darkTheme);
+  const load = useSelector(loading);
 
   console.log(userData);
+  console.log(dark);
 
   useEffect(() => {
+    dispatch(changeTheme( false || JSON.parse(localStorage.getItem('darkTheme')) ));
+
     setTimeout(() => { onExpired(true); document.body.style.overflow = "hidden"}, 12*60*60*1000)
   }, [dispatch]);
   const [expired, onExpired] = useState(false);
@@ -23,9 +29,11 @@ export const MainPage = () => {
     : `${styles.mainpage}`
 
   return (
-    <section className={mainpageStyle} >
-      <TopBar/>
-      { expired ? <ExpirationScreen/> : null }
-    </section>
+    load
+    ? <LoadPage/>
+    : <section className={mainpageStyle} >
+        <TopBar/>
+        { expired ? <ExpirationScreen/> : null }
+      </section>   
   )
 }
