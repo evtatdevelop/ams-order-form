d7a4bf5b71bf94c901ffe56aa6cdb65fa739421d
Evgenii Tatarenko <tatarenkoe@gmail.com>
2024-09-13 17:00:30 +0700

    DatteInterval features

--- src/features/components/inputDate/inputDate.js
+++ src/features/components/inputDate/inputDate.js
@@ -1,49 +1,58 @@
 /* eslint-disable no-useless-escape */
-import React, {useState, useRef, } from "react";
+import React, {useState, useRef, useEffect, } from "react";
 import styles from './inputDate.module.scss';
 import DatePicker from "./datePicker";
 
 export const InputDate = props => {
   const ref = useRef(null)
-  const {dateHandler, lang} = props
-  
-  const [value, setValue] = useState("")
+  const {dateHandler, lang, val} = props
+
+  const [value, setValue] = useState('')
   const [jsDate, setJsDate] = useState(null)
   const [showPicker, setShowPicker] = useState(false)
 
-  const outClick = (e) => {
-    console.log(e.target.closest('#picker'));
-    if ( !e.target.closest('#picker') ) {
-      setShowPicker(false)
-      document.removeEventListener("mouseup", outClick);
+  useEffect(() => { 
+    
+    if ( !val ) {
+      console.log('useEffect', getLocalDate(val));
+      setValue(''); 
+      setJsDate(null)      
     }
-  }
+; 
 
-  const onShowPicker = () => {    
-    document.addEventListener("mouseup", outClick);
-    setShowPicker(!showPicker);
-  }
+   },[val])
 
-  const onSetDate = date => {
-    if ( !date ) { setValue(''); setJsDate(null); return }
-    setJsDate(date)
+  const getLocalDate = date => {
+    if ( !date ) return '';
     const dd = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`
     const mm = date.getMonth()+1 > 9 ? date.getMonth()+1 : `0${date.getMonth()+1}`
-    setValue( lang.toUpperCase() === 'RU' ? `${dd}.${mm}.${date.getFullYear()}` : `${dd}-${mm}-${date.getFullYear()}`)
-    dateHandler(date)
+    return lang.toUpperCase() === 'RU' ? `${dd}.${mm}.${date.getFullYear()}` : `${dd}-${mm}-${date.getFullYear()}`;
+  };
+
+  const onSetDate = date => {
+    if ( !date ) { 
+      dateHandler(null);
+      setValue(''); 
+      setJsDate(null); 
+      return 
+    }
+    setJsDate(date);
+    dateHandler(date);
+    setValue( getLocalDate(date) )
   }
 
   const onInput = val => {
     setShowPicker(false)
-    let dateVal = val.replace(/[^0-9\.\/-: ]/ig, "")
-    dateVal = dateVal.replace(/\.{2,}/ig, ".")
-    dateVal = dateVal.replace(/\/{2,}/ig, "/")
-    dateVal = dateVal.replace(/-{2,}/ig, "-")
+    let dateVal = val
+      .replace(/[^0-9\.\/-: ]/ig, "")
+      .replace(/\.{2,}/ig, ".")
+      .replace(/\/{2,}/ig, "/")
+      .replace(/-{2,}/ig, "-");
     setValue(dateVal);
   }
 
   const onBlur = () => {
-    onSetDate(getInputDate());
+    // onSetDate(getInputDate());
   }
 
   const getInputDate = () => {
@@ -89,6 +98,19 @@ export const InputDate = props => {
       default:
         break;
     } 
+  }
+
+
+  const outClick = (e) => {
+    if ( !e.target.closest('#picker') ) {
+      setShowPicker(false)
+      document.removeEventListener("mouseup", outClick);
+    }
+  }
+
+  const onShowPicker = () => {    
+    document.addEventListener("mouseup", outClick);
+    setShowPicker(!showPicker);
   }
 
   const localMask = lang.toUpperCase() === 'RU' ? 'дд.мм.гггг' : 'dd-mm-yyyy'
--- src/features/corpsystems/roles/roleSandbox/dateInterval/dateInterval.js
+++ src/features/corpsystems/roles/roleSandbox/dateInterval/dateInterval.js
@@ -9,22 +9,43 @@ import { InputDate } from "../../../../components/inputDate/inputDate";
 export const DateInterval = () => {
   const { lang, } = useSelector(user);
 
-  
+
+  const [from, setFrom] = useState(null);
+  const [to, setTo] = useState(null);
+
+  //toDo: !!!
+  const intervalLogic = ( item, val ) => {
+    console.log(item, val);
+    if ( item === 'from' ) {
+      setFrom(val);
+      if ( (to-val)/1000/3600/24 < 0 ) {
+        setTo(null);
+      }      
+    }
+    if ( item === 'to' ) {
+      setTo(val);
+      if ( (val-from)/1000/3600/24 < 0 ) {
+        setFrom(null);
+      }
+    }
+  }
 
   return (
     <div className={styles.dateInterval}>
       <div className={styles.partInterval}>
         <div className={styles.intervalName}>From</div>
         <InputDate
-          dateHandler = { val => console.log(val) }
+          dateHandler = { val => intervalLogic('from', val) }
           lang = {lang}
+          val = {from}
         />        
       </div>
       <div className={styles.partInterval}>
         <div className={styles.intervalName}>To</div>
           <InputDate
-            dateHandler = { val => console.log(val) }
-            lang = {lang} 
+            dateHandler = { val => intervalLogic('to', val) }
+            lang = {lang}
+            val = {to}
           />      
       </div>
 
